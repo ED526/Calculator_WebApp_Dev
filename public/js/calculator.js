ea4c9f2f927c35786cf1c604248843017575d64d
ED526 <eddieraydeleon526@gmail.com>
2022-07-26 23:44:18 -0500

    CSS
    CSS

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -12,3 +12,32 @@ function calculateResult(){
 
     document.getElementById("result").value = result
 }
+
+function posneg(){
+    let num = document.getElementById("result").value
+    let result = eval(num * -1)
+
+    document.getElementById("result").value = result
+
+}
+
+function power(){
+    let num = document.getElementById("result").value
+    let result = eval(Math.pow(num, num))
+
+    document.getElementById("result").value = result
+}
+
+function square(){
+    let num = document.getElementById("result").value
+    let result = eval(num * num)
+
+    document.getElementById("result").value = result
+}
+
+function percentage(){
+    let num = document.getElementById("result").value
+    let result = eval(num / 100)
+
+    document.getElementById("result").value = result
+}
